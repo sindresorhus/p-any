2ddcb685d96a7e4653a7177400624716ce056cb6
Sindre Sorhus <sindresorhus@gmail.com>
2016-11-25 23:46:34 +0700

    fix typo

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import delay from 'delay';
 import m from './';
 
-test('returns the first fullfilled value', async t => {
+test('returns the first fulfilled value', async t => {
 	const f = [
 		Promise.reject(1),
 		Promise.resolve(2),
@@ -12,7 +12,7 @@ test('returns the first fullfilled value', async t => {
 	t.deepEqual(await m(f), 2);
 });
 
-test('returns the first fullfilled value #2', async t => {
+test('returns the first fulfilled value #2', async t => {
 	const f = [
 		Promise.resolve(1).then(delay(100)),
 		Promise.resolve(2).then(delay(10)),
